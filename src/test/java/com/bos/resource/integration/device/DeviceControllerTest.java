8982e184a5f9668e8d9867e3941bde014a1fb7ab
mclee <myoungcheol.lee@bluebirdcorp.com>
2024-02-05 12:22:09 +0900

    impl: v1/asset integration test

--- src/test/java/com/bos/resource/integration/device/DeviceControllerTest.java
+++ src/test/java/com/bos/resource/integration/device/DeviceControllerTest.java
@@ -1,6 +1,7 @@
 package com.bos.resource.integration.device;
 
 import com.bos.resource.app.device.controller.DeviceController;
+import com.bos.resource.app.resourceowner.exception.ResourceOwnerErrorCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -48,7 +50,7 @@ public class DeviceControllerTest {
 //            final String username = "dev_admin";
             final String username = "fake_user1";
             final String message = "success";
-            final String model = "EF501";
+            final String model = "HF550";
             final String serialNumber = "HF550XANLCBA001";
 
             mockMvc.perform(get("/v1/asset")
@@ -64,6 +66,28 @@ public class DeviceControllerTest {
                     .andExpect(jsonPath("$.data.assets").isNotEmpty())
                     .andExpect(jsonPath("$.data.assets[0].model").value(model))
                     .andExpect(jsonPath("$.data.assets[0].serialNumber").value(serialNumber));
+        }
+
+        @Test
+        @DisplayName("Test asset API - 200 OK")
+        public void testAsset_fail_notExistsUser() throws Exception {
+            final String username = "not_exists_user1";
+            final String message = "success";
+            final String model = "HF550";
+            final String serialNumber = "HF550XANLCBA001";
+
+            mockMvc.perform(get("/v1/asset")
+                            .with(jwt()
+                                    .jwt(jwt -> jwt.claim(StandardClaimNames.SUB, username)))
+                    )
+                    .andDo(print())
+                    .andExpect(status().isNotFound())
+                    .andExpect(handler().handlerType(DeviceController.class))
+                    .andExpect(handler().methodName("asset"))
+                    .andExpect(jsonPath("$.msg").value(ResourceOwnerErrorCode.RESOURCE_OWNER_NOT_FOUND.getMsg()))
+                    .andExpect(jsonPath("$.httpCode").value(HttpStatus.NOT_FOUND.value()))
+                    .andExpect(jsonPath("$.internalCode").value(ResourceOwnerErrorCode.RESOURCE_OWNER_NOT_FOUND.getBizCode()))
+                    .andExpect(jsonPath("$.timestamp").isNotEmpty());
         }
     }
 }
